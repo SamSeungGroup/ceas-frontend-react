f76b8301d5d9d28c72bf3da1d6be17df55fd1995
songjungwoo <sjw9664@naver.com>
2023-03-27 13:46:56 +0900

    '상품 목록 위치 오류' 수정

--- src/pages/product-list/ProductList.js
+++ src/pages/product-list/ProductList.js
@@ -167,6 +167,29 @@ const ProductList = () => {
                 <div className = "product-list-title">상품 목록</div>
             </div>
 
+            <div className = "hot-product-list-instruction">
+                <img className = "hot-product_image" src = "../../image/hot-product_icon.png"/>
+                <h2 className = "hot-product-list-title">Top3 긍정 상품</h2>
+            </div>
+
+            <div className = "hot-product-list_body">
+                {
+                    onList ? (hotProductList.map((item, index) => (
+                        <ProductItemCard
+                            key = { index } 
+                            productId = { item.id }
+                            productImage = { `http://localhost:8080/images/product/${item.id}` }
+                            productName = { item.productName }
+                            productPrice = { item.productPrice }
+                            productPositive = { item.productPositive }
+                            userName = { item.seller.userName }
+                            productCreateDate = { moment(item.created).format('YYYY년 MM월 DD일') }
+                        />
+                ))) : (
+                    <h2 className = "no-hot-product-list">등록된 인기 상품이 없습니다.</h2>
+                )}
+            </div>
+
             <div className = "search-product_wrapper">
                 <TextField 
                     className = "search-product_textarea" 
@@ -203,29 +226,6 @@ const ProductList = () => {
                             <h2 className = "no-product-list">등록된 상품이 없습니다.</h2>
                         )
                 }
-            </div>
-
-            <div className = "hot-product-list-instruction">
-                <img className = "hot-product_image" src = "../../image/hot-product_icon.png"/>
-                <h2 className = "hot-product-list-title">Top3 긍정 상품</h2>
-            </div>
-
-            <div className = "hot-product-list_body">
-                {
-                    onList ? (hotProductList.map((item, index) => (
-                        <ProductItemCard
-                            key = { index } 
-                            productId = { item.id }
-                            productImage = { `http://localhost:8080/images/product/${item.id}` }
-                            productName = { item.productName }
-                            productPrice = { item.productPrice }
-                            productPositive = { item.productPositive }
-                            userName = { item.seller.userName }
-                            productCreateDate = { moment(item.created).format('YYYY년 MM월 DD일') }
-                        />
-                ))) : (
-                    <h2 className = "no-hot-product-list">등록된 인기 상품이 없습니다.</h2>
-                )}
             </div>
 
             <div className = "product-list_footer">
