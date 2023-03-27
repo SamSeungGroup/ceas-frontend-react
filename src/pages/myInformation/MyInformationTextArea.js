edfcd99cc7a8857f938b24bbd9a9cd97e9718265
songjungwoo <sjw9664@naver.com>
2023-03-27 15:04:04 +0900

    '내 정보 수정'에서 'IMP', 'PG 코드'를 입력하도록 설정

--- src/pages/myInformation/MyInformationTextArea.js
+++ src/pages/myInformation/MyInformationTextArea.js
@@ -7,7 +7,7 @@ import "./myinformationtextarea.scss";  // myinformationtextarea.scss 모듈: '�
 
 /* 2. 함수 설정 */
 // TextArea 함수: '상품 이름, 상품 설명' 작성 기능 구현 + 화면 표시
-const MyInformationTextArea = ({ setUserName, userName, setUserEmail, userEmail }) => {                                                                                                                       
+const MyInformationTextArea = ({ setUserName, userName, setUserEmail, userEmail, setIMP, IMP, setPG, PG }) => {                                                                                                                       
     // [1] 처리
     // 화면 렌더링
     return (
@@ -30,6 +30,26 @@ const MyInformationTextArea = ({ setUserName, userName, setUserEmail, userEmail 
                     className = "user-name"
                     placeholder = "이메일을 입력하세요."
                     value = { userEmail }
+                />
+
+            <label>* IMP(가맹점 식별코드) 등록: </label>
+                <input
+                    onChange = {(e) => {
+                        setIMP(e.target.value);
+                    }}
+                    className = "user-name"
+                    placeholder = "IMP를 입력하세요."
+                    value = { IMP }
+                />
+
+            <label>* PG사 코드 등록: </label>
+                <input
+                    onChange = {(e) => {
+                        setPG(e.target.value);
+                    }}
+                    className = "user-name"
+                    placeholder = "PG사 코드을 입력하세요."
+                    value = { PG }
                 />
         </div>
     );
